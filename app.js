240685d12a489b01ca7b5d7d20e5418697e85360
Veljko03 <veljkodovragovic@gmail.com>
2024-10-12 08:42:08 +0200

    changed node to express

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-const http = require("http");
-const fs = require("fs");
-const { log } = require("console");
+const express = require("express");
 
-const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/html");
+const app = express();
 
-  let path = "./views/";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      break;
-    case "/about":
-      path += "about.html";
-      break;
-    case "/contact-me":
-      path += "contact-me.html";
-      break;
-    default:
-      path += "404.html";
-      break;
-  }
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.log(err);
-      res.end();
-    }
-    res.statusCode = 200;
-    res.end(data);
-  });
+app.get("/", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
 });
 
-server.listen(3000, "localhost", () => {
+app.get("/about", (req, res) => {
+  res.sendFile("./views/about.html", { root: __dirname });
+});
+
+app.get("/contact-me", (req, res) => {
+  res.sendFile("./views/contact-me.html", { root: __dirname });
+});
+
+app.use((req, res) => {
+  res.sendFile("./views/404.html", { root: __dirname });
+});
+
+app.listen(3000, "localhost", () => {
   console.log("listening to port 3000");
 });
